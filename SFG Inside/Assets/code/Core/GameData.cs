c63ead7b63646455b88a1db2f709709168ba06eb
KarZodrak <blacklion33@gmx.ch>
2018-04-12 11:27:23 +0200

    added dialog system and npc

--- SFG Inside/Assets/code/Core/GameData.cs
+++ SFG Inside/Assets/code/Core/GameData.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System.Collections.Generic;
+
+/*
  * GameData
  * 
  * GameData stores core relevant data.
@@ -9,4 +11,9 @@ public class GameData
     public string levelName = "hub_zone";
 	public bool gamePaused = false;
 	public bool playerWins = true;
+
+    //dialogs
+    public int activeDialogIndex = 0;
+    public string speakerName = "";
+    public List<string> activeDialog = new List<string>();
 }
