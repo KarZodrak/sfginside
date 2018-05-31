b86685d1ea39f8b12a8e23e6e915a6089b6d9482
KarZodrak <blacklion33@gmx.ch>
2018-05-31 10:54:39 +0200

    update interaction

--- SFG Inside/Assets/code/PlayerInteraction.cs
+++ SFG Inside/Assets/code/PlayerInteraction.cs
@@ -11,69 +11,68 @@ using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
-	private Item _itemAtHand = null;
+    public float interactionRadius = 4.0f;
 
-	//unity update
-	public void Update () 
+    public void Start()
+    {
+        //hide cursor
+        //Cursor.visible = false;
+    }
+
+    //unity update
+    public void Update () 
 	{
-		// fetch item at hand, if there is any (function returns 'null' if theres none)
-		_itemAtHand = GameLogic.game.inventory.getItemInHand();
-
 		// on mouse click up if game is not paused
-		if(!GameLogic.game.data.gamePaused && Input.GetMouseButtonUp(0))
+		if(!GameLogic.game.data.gamePaused && Input.GetKeyDown(KeyCode.E))
 		{
-			// cast ray from mouse position into the world
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;
-			if (Physics.Raycast(ray, out hit))
-			{
-				// check if we clicked on an item
-				if (hit.collider.tag == "Item")
-				{
-					// fetch item component
-					Item clickedItem = hit.transform.GetComponent<Item>();
-					if (_itemAtHand == null)
-					{
-						// pickup item
-						clickedItem.pickUp(gameObject);
-					}
-				}
-                // check if we clicked on an npc
-                if (hit.collider.tag == "NPC")
+            GameObject interactionObject = null;
+            float distOfClosestObject = Mathf.Infinity;
+            RaycastHit[] hits = Physics.SphereCastAll(transform.position, interactionRadius, transform.forward, 0.1f);
+
+            foreach (RaycastHit hit in hits)
+            {
+                if (hit.collider.tag == "Item" || hit.collider.tag == "NPC" || hit.collider.tag == "Riddler" || hit.collider.tag == "InteractionObject")
                 {
-                    // get interaction component and execute its interaction
-                    hit.transform.GetComponent<NPC>().startDialog(gameObject);
-                }
-                else if (hit.collider.tag == "Riddler")
-                {
-                    // get interaction component and execute its interaction
-                    hit.transform.GetComponent<Riddler>().startDialog(gameObject);
-                }
-                //check if we clicked on an interaction object
-                else if (hit.collider.tag == "InteractionObject")
-				{
-                    // get interaction component and execute its interaction
-                    if (!hit.transform.GetComponent<InteractionObject>().isTrigger)
+                    if (interactionObject == null)
                     {
-                        hit.transform.GetComponent<InteractionObject>().doAction(gameObject, _itemAtHand);
+                        interactionObject = hit.collider.gameObject;
+                        distOfClosestObject = Vector3.Distance(transform.position, interactionObject.transform.position);
                     }
-				}
-				//chek if we hit not the player
-				else if (hit.collider.tag != "Player")
-				{
-					//if we are dragging an item
-					if (_itemAtHand != null)
-					{
-						// calculate item drop point
-						Vector3 offset = new Vector3 (
-							hit.normal.x * _itemAtHand.GetComponent<Collider>().bounds.extents.x,
-							hit.normal.y * _itemAtHand.GetComponent<Collider>().bounds.extents.y,
-							hit.normal.z * _itemAtHand.GetComponent<Collider>().bounds.extents.z
-							);
-						// drop item
-						_itemAtHand.place(hit.point + offset + Vector3.up*2.0f, hit.transform);
-					}
-				}
+                    else
+                    {
+                        if (distOfClosestObject > Vector3.Distance(transform.position, hit.collider.transform.position))
+                        {
+                            interactionObject = hit.collider.gameObject;
+                        }
+                    }
+                }
+            }
+
+            // check if we interacted with an item
+            if (interactionObject.tag == "Item")
+			{
+                // pickup item
+                interactionObject.transform.GetComponent<Item>().pickUp(gameObject);
+			}
+            // check if we interacted with a npc
+            else if (interactionObject.tag == "NPC")
+            {
+                // get interaction component and execute its interaction
+                interactionObject.transform.GetComponent<NPC>().startDialog(gameObject);
+            }
+            else if (interactionObject.tag == "Riddler")
+            {
+                // get interaction component and execute its interaction
+                interactionObject.transform.GetComponent<Riddler>().startDialog(gameObject);
+            }
+            //check if we interacted with an interaction object
+            else if (interactionObject.tag == "InteractionObject")
+			{
+                // get interaction component and execute its interaction
+                if (!interactionObject.transform.GetComponent<InteractionObject>().isTrigger)
+                {
+                    interactionObject.transform.GetComponent<InteractionObject>().doAction(gameObject);
+                }
 			}
 		}
 	}
